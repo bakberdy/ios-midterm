f635a4abe911cf2ec67626bc26d257c87e09932e
Bakberdi Yessentay <bakberdi.yessentay@bcchub.kz>
2026-03-11 14:08:26 +0500

    Initial Commit

---
+++ ios-midtermTests/ios_midtermTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_midtermTests.swift
+//  ios-midtermTests
+//
+//  Created by Есентай Бақберді Муханұлы on 11.03.2026.
+//
+
+import Testing
+@testable import ios_midterm
+
+struct ios_midtermTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
